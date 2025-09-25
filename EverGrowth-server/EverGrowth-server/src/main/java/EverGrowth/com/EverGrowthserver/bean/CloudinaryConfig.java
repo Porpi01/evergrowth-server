0174627e9ffe9dc7d7c9d63ae136e98537c4bf7e
Porpi01 <macanyiz@gmail.com>
2025-09-25 09:03:23 +0200

    variables en produccion

--- EverGrowth-server/EverGrowth-server/src/main/java/EverGrowth/com/EverGrowthserver/bean/CloudinaryConfig.java
+++ EverGrowth-server/EverGrowth-server/src/main/java/EverGrowth/com/EverGrowthserver/bean/CloudinaryConfig.java
@@ -1,5 +1,7 @@
 package EverGrowth.com.EverGrowthserver.bean;
 
+import java.io.File;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,14 +13,28 @@ import io.github.cdimascio.dotenv.Dotenv;
 @Configuration
 public class CloudinaryConfig {
 
-    @Bean
-    public Cloudinary cloudinary() {
-        // Carga las variables del archivo .env
+@Bean
+public Cloudinary cloudinary() {
+    String cloudName;
+    String apiKey;
+    String apiSecret;
+
+    File envFile = new File(".env");
+    if (envFile.exists()) {
         Dotenv dotenv = Dotenv.load();
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
-                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
-                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")
-        ));
+        cloudName = dotenv.get("CLOUDINARY_CLOUD_NAME");
+        apiKey = dotenv.get("CLOUDINARY_API_KEY");
+        apiSecret = dotenv.get("CLOUDINARY_API_SECRET");
+    } else {
+        cloudName = System.getenv("CLOUDINARY_CLOUD_NAME");
+        apiKey = System.getenv("CLOUDINARY_API_KEY");
+        apiSecret = System.getenv("CLOUDINARY_API_SECRET");
     }
+
+    return new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", cloudName,
+            "api_key", apiKey,
+            "api_secret", apiSecret
+    ));
+}
 }
